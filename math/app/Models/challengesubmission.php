66c9ec5f73c432fcf3710e6e7e7a14536a6b3ef6
mugisachantal <mjojassen94@gmail.com>
2024-07-26 13:03:50 +0300

    i have edited the home and bestwo views

--- math/app/Models/challengesubmission.php
+++ math/app/Models/challengesubmission.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class challengesubmission extends Model
-<<<<<<< HEAD
-{  protected $table ='challengesubmission';
-=======
 {
     protected $table = 'challengesubmission'; 
->>>>>>> 8ff2b3ea78d98eb2aa34ce6e6a46caa56b125bb0
     use HasFactory;
 }
